e98bac574093ce20c7e3b9c805abfabb97ca1c87
Afvanie <ardyansah201@gmail.com>
2022-12-07 11:35:17 +0700

    first commit

---
+++ app/Http/Controllers/LaporanUserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\RincianBiaya;
+use PDF;
+
+class LaporanUserController extends Controller
+{
+    public function index()
+    {
+        return view('laporanUser.index');
+    }
+
+    public function cetak(Request $request)
+    {
+        if($request->tipe == 'rincianbiaya'){
+            $rincianbiaya = RincianBiaya::all();
+
+            $pdf = PDF::loadview('laporanUser.rincianbiaya',['rincianbiaya'=>$rincianbiaya]);
+            return $pdf->download('laporan-rincianbiaya.pdf');
+        }
+        else {
+            $transaksi = DataTransaksi::all();
+
+            $pdf = PDF::loadview('laporan.transaksi',['transaksi'=>$transaksi]);
+            return $pdf->stream('laporan-transaksi.pdf');
+        }
+    }
+}
